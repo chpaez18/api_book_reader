f29f872c0646353f281ae2ce3ecba73e8cf78af0
Christian <christianpaez1029@gmail.com>
2023-05-22 19:53:13 +0200

    update

--- config/services.php
+++ config/services.php
@@ -44,11 +44,11 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URL'),
-        'verify' => false, // Agrega esta línea para deshabilitar la verificación de SSL
-        'access_type'   => 'offline'
-        /* 'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+        //'verify' => false, // Agrega esta línea para deshabilitar la verificación de SSL
+        'access_type'   => 'offline',
+        'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
         'token_uri' => 'https://accounts.google.com/o/oauth2/token',
-        'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs', */
+        /* 'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs', */
     ],
 
     'passport' => [
